dd7a7234095f5469206bd15726644c83f3828801
dcabrerar <deivy_cabrera@usmp.pe>
2025-01-30 12:29:12 -0500

    300120251229

--- sensor_server.py
+++ sensor_server.py
@@ -29,7 +29,8 @@ async def server(websocket, path):
             await websocket.send('Error: '+str(e))
             
 # Iniciar el servidor WebSocket
-start_server = websockets.serve(server, "localhost", 8765)
+#start_server = websockets.serve(server, "localhost", 8765)
+start_server = websockets.serve(server, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
